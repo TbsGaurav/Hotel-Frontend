5f5d572da988241329de8bd0eee476c0e217902c
VrushikaM <tbs.vrushika@gmail.com>
2026-02-16 18:41:39 +0530

    test

--- components/sections/HeroSection.jsx
+++ components/sections/HeroSection.jsx
@@ -62,6 +62,7 @@ export default function HeroSection() {
             try {
                 setLoading(true);
                 const data = await globalSearchapi(query);
+                console.log(data);
                 setResults(data || []);
                 setShow(true);
             } catch (e) {
