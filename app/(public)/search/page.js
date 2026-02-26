8a994d3cace398e1f4fe58cef72496ec0f190b9a
VrushikaM <tbs.vrushika@gmail.com>
2026-02-26 16:32:23 +0530

    refactor search page structure and implement SearchContent component

--- app/(public)/search/page.js
+++ app/(public)/search/page.js
@@ -1,32 +1,10 @@
-'use client';
+import { Suspense } from 'react';
+import SearchContent from './SearchContent';
 
-import { useSearchParams } from 'next/navigation';
-
-export default function SearchPage() {
-    const searchParams = useSearchParams();
-
-    const data = {
-        destination: searchParams.get('destination'),
-        destinationType: searchParams.get('destinationType'),
-        destinationId: searchParams.get('destinationId'),
-
-        checkIn: new Date(searchParams.get('checkIn')),
-        checkOut: new Date(searchParams.get('checkOut')),
-
-        guests: Number(searchParams.get('guests')),
-        rooms: Number(searchParams.get('rooms')),
-
-        children: {
-            count: Number(searchParams.get('children')),
-            ages: searchParams.get('childrenAges') ? searchParams.get('childrenAges').split(',').map(Number) : []
-        }
-    };
-
-
+export default function Page() {
     return (
-        <div style={{ padding: 40 }}>
-            <h3>Search Results Page</h3>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
+        <Suspense fallback={<div>Loading search...</div>}>
+            <SearchContent />
+        </Suspense>
     );
 }
